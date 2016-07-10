79030aa80d8329f9903da7601d56145c6f04630f
randy1200 <randy1200@yahoo.com>
2016-07-09 20:09:34 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -14,6 +14,36 @@
 </head>
 <body>
 	<img id="logo" src="logo_blue.png" alt="logo.png"/>
-	<h1><?php echo "Hello, World!!"; ?></h1>
+
+	<?php
+
+	$hamburgerPrice = 4.95;
+	$milkshakePrice = 1.95;
+	$colaPrice = .85;
+	$tax = .075;
+	$tip = .016;
+
+	print 'Prices';
+	print '';
+	print 'Hamburger: ' . $hamburgerPrice;
+	print 'Milk Shake: ' . $milkshakePrice;
+	print 'Cola: ' . $colaPrice;
+	print 'Tax: ' . $tax;
+	print 'Tip: ' . $tip;
+	print '';
+
+	$totalFood = $hamburgerPrice + $hamburgerPrice + $milkshakePrice + $colaPrice;
+	$pretaxTip = $totalFood * $tax;
+	$taxOnFood = $totalFood * $tax;
+
+	print 'Lunch Tab';
+	print 'Food Total: ' . $totalFood;
+	print 'Pretax Tip: ' . $pretaxTip;
+	print 'Tax On Food: ' . $taxOnFood;
+	print 'Total: ' . $totalFood + $pretaxTip + $taxOnFood;
+
+
+	?>
+
 </body>
 </html>
